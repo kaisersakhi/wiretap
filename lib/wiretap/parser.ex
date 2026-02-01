d5c20c848eb00bb01020d53677a8a7bd79a701e1
Kaiser Sakhi <mail@kaisersakhi.com>
2026-02-01 16:36:14 +0530

    Parse headers and body

--- lib/wiretap/parser.ex
+++ lib/wiretap/parser.ex
@@ -1,4 +1,10 @@
 defmodule Wiretap.Parser do
+  # <method> <request-target> <http-version>\r\n
+  # <header-name>: <header-value>\r\n
+  # <header-name>: <header-value>\r\n
+  # ...\r\n
+  # <message-body>
+
   @type t :: %__MODULE__{
           state: :headers | :body | :done,
           buffer: binary(),
@@ -10,7 +16,7 @@ defmodule Wiretap.Parser do
     state: :headers,
     buffer: "",
     headers: nil,
-    content_length: 0
+    content_length: 0,
   ]
 
   @spec new() :: t()
@@ -22,26 +28,65 @@ defmodule Wiretap.Parser do
   def feed(%__MODULE__{} = parser, data) do
     parser = %{parser | buffer: parser.buffer <> data}
 
-    case parser.state do
+    parser = case parser.state do
       :headers -> parse_headers(parser)
       :body -> parse_body(parser)
       _ -> parser
     end
+
+    parser
   end
 
   defp parse_headers(%__MODULE__{state: :headers} = parser) do
     case String.split(parser.buffer, "\r\n\r\n", parts: 2) do
-      [_headers, _body] ->
-        # TODO: Implement header parsing
-        parser
+      [headers_str, _body_str] ->
+        headers = parse_headers_str(headers_str)
+        %{parser | headers: headers, state: :body, content_length: headers["content-length"]}
 
       [_incomplete] ->
         parser
     end
   end
 
+  @spec parse_headers_str(binary()) :: %{
+          :http_version => binary(),
+          :method => binary(),
+          :path => binary(),
+          optional(any()) => any()
+        }
+  def parse_headers_str(headers_str) do
+    [request_line | header_lines] = String.split(headers_str, "\r\n")
+
+    [method, path, http_version] = String.split(request_line, " ", parts: 3)
+
+    header_lines
+    |> Enum.reduce(%{}, fn line, acc ->
+      case String.split(line, ":", parts: 2) do
+        [key, value] ->
+          Map.put(acc, String.downcase(key), String.trim(value))
+        _ -> acc
+      end
+    end)
+    |> Map.put("method", method)
+    |> Map.put("path", path)
+    |> Map.put("http_version", http_version)
+  end
+
   defp parse_body(%__MODULE__{state: :body} = parser) do
-    # TODO: Implement body parsing
-    parser
+    body = parser.buffer |> String.split("\r\n\r\n", parts: 2) |> List.last()
+
+    if byte_size(body) == parser.content_length |> String.to_integer() do
+      request = %Wiretap.Request{
+        method: parser.headers["method"],
+        path: parser.headers["path"],
+        version: parser.headers["http_version"],
+        headers: parser.headers,
+        body: body
+      }
+
+      {:done, request, "", %{parser | state: :done}}
+    else
+      parser
+    end
   end
 end
--- test/wiretap/parser_test.exs
+++ test/wiretap/parser_test.exs
@@ -34,4 +34,44 @@ defmodule Wiretap.ParserTest do
       assert parser.buffer == "GET / HTTP/1.1\r\n"
     end
   end
+
+  describe "parse_headers/1" do
+    test "parses headers" do
+      parser = Parser.new()
+      parser = Parser.feed(parser, "GET / HTTP/1.1\r\nHost: example.com\r\n")
+      parser = Parser.feed(parser, "User-Agent: Elixir\r\n")
+      parser = Parser.feed(parser, "Content-Len")
+      parser = Parser.feed(parser, "gth: 0\r\n")
+      parser = Parser.feed(parser, "\r\n")
+
+      assert parser.headers == %{
+        "host" => "example.com",
+        "method" => "GET",
+        "path" => "/",
+        "http_version" => "HTTP/1.1",
+        "user-agent" => "Elixir",
+        "content-length" => "0"
+      }
+      assert parser.content_length == "0"
+      assert parser.state == :body
+    end
+  end
+
+  describe "parse_body/1" do
+    test "parses body" do
+      parser = Parser.new()
+      parser = Parser.feed(parser, "GET / HTTP/1.1\r\nHost: example.com\r\n")
+      parser = Parser.feed(parser, "User-Agent: Elixir\r\n")
+      parser = Parser.feed(parser, "Content-Len")
+      parser = Parser.feed(parser, "gth: 13\r\n")
+      parser = Parser.feed(parser, "\r\n")
+      {:done, request, rest, parser} = Parser.feed(parser, "Hello, World!")
+
+      assert request.body == "Hello, World!"
+      assert parser.state == :done
+    end
+  end
+
+
+
 end
